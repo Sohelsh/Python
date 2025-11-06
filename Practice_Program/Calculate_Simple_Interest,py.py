0011bb97016cc07cedb2d413a2a6afd298527943
Sohelsh <sohelshaikhadam@gmail.com>
2025-11-06 23:37:18 +0530

    Practice

---
+++ Practice_Program/Calculate_Simple_Interest,py.py
@@ -0,0 +1,7 @@
+Total = int(input("Enter Total Amount: "))
+Rate = int(input("Enter Rate(Percentage): "))
+Years =int(input("Enter Years: "))
+
+Interest = (Total * Rate * Years)/100
+
+print("Total Interest is :",Interest)
